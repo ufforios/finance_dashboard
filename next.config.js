2ab9df889778d9b07dd7fa14c036356a94338d10
Ulises <uf.forios@gmail.com>
2025-12-01 00:47:28 -0300

    5commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("@ducanh2912/next-pwa").default({
-    dest: "public",
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === "development",
-});
-
 const nextConfig = {
     reactStrictMode: true,
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
